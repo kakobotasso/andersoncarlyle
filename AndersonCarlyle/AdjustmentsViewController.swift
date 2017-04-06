6cc2162df31175b34d83f76d249a8fdccf05d454
Kako Botasso <kakodev@gmail.com>
2017-04-05 22:02:00 -0300

    Cria alert para adicionar estado

--- AndersonCarlyle/AdjustmentsViewController.swift
+++ AndersonCarlyle/AdjustmentsViewController.swift
@@ -10,8 +10,33 @@ import UIKit
 
 class AdjustmentsViewController: UIViewController {
 
+    // MARK: - Super methods
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    @IBAction func addNewState(_ sender: UIButton) {
+        showAlert()
+    }
+    
+    // MARK: - Methods
+    func showAlert(){
+        let alert = UIAlertController(title: "Adicionar Estado", message: nil, preferredStyle: .alert)
+        
+        alert.addTextField { (textField: UITextField) in
+            textField.placeholder = "Nome do estado"
+        }
+        
+        alert.addTextField { (textField: UITextField) in
+            textField.placeholder = "Imposto"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Adicionar", style: .default, handler: { (action: UIAlertAction) in
+            //TODO: Criar lógica de insert
+        }))
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 
 }
