80caaffd93c702d3a9553d03fadd84339660784c
Kako Botasso <kakodev@gmail.com>
2017-04-05 21:41:39 -0300

    Cria classes das telas

---
+++ AndersonCarlyle/AdjustmentsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AdjustmentsViewController.swift
+//  AndersonCarlyle
+//
+//  Created by Usuário Convidado on 05/04/17.
+//  Copyright © 2017 Kako Botasso & Anderson Macedo. All rights reserved.
+//
+
+import UIKit
+
+class AdjustmentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ AndersonCarlyle/TotalPriceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TotalPriceViewController.swift
+//  AndersonCarlyle
+//
+//  Created by Usuário Convidado on 05/04/17.
+//  Copyright © 2017 Kako Botasso & Anderson Macedo. All rights reserved.
+//
+
+import UIKit
+
+class TotalPriceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
