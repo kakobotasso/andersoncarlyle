17f848588f6794a3e65e7f27b197501d1eb25974
dev4anderson <nds.anderson@gmail.com>
2017-04-14 18:04:50 -0300

    Data Model e Outlet cadprod

---
+++ AndersonCarlyle/ProductRegisterViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProductRegisterViewController.swift
+//  AndersonCarlyle
+//
+//  Created by Anderson Macedo on 14/04/17.
+//  Copyright © 2017 Kako Botasso & Anderson Macedo. All rights reserved.
+//
+
+import UIKit
+
+class ProductRegisterViewController: UIViewController {
+
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var ivProd: UIImageView!
+    @IBOutlet weak var tfState: UITextField!
+    @IBOutlet weak var tfPrice: UITextField!
+    @IBOutlet weak var swCard: UISwitch!
+    @IBOutlet weak var btRegister: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
